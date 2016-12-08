12191da6cd82363fcbe107a7e059b0e601a6978b
Sweta Shrestha <sweta.shrestha@yipl.com.np>
2016-12-08 17:31:43 +0545

    Activity Viewer

--- resources/views/perfectViewer/activity-viewer.blade.php
+++ resources/views/perfectViewer/activity-viewer.blade.php
@@ -1,1 +1,282 @@
-<h1> This is activity page.</h1>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+    <title>Activity Viewer</title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<div class="wrapper">
+    <header>Header</header>
+    <section class="col-md-12 org-map-wrapper">
+        <div class="width-940">
+            <div class="organisation-info">
+                <a href="#" class="organisation-logo">
+                    <!--dynamic organisation logo-->
+                    <img src="images/org-logo.png" alt="AbleChildAfrica" width="238" height="68">
+                </a>
+                <span class="organisation-name">
+                    <a href="#" title="AbleChildAfrica">
+                        <!--dynamic organisation name-->
+                        AbleChildAfrica
+                    </a>
+                </span>
+                <address><i class="pull-left material-icons">room</i>Unit 3 Graphite Square, London, SE11 5EE</address>
+                <a href="#" class="see-all-activities"><i class="pull-left material-icons">arrow_back</i>See all
+                    Activities</a>
+            </div>
+        </div>
+    </section>
+    <section class="col-md-12 activity-main-wrapper">
+        <div class="width-940">
+            <div class="col-xs-12 activity-detail-wrapper">
+                <div class="activity-detail-top-wrapper">
+                    <h1>
+                        <!--dynamic title-->
+                        A Child to Child approach to inclusive education in Uganda
+                    </h1>
+                    <div class="activity-iati-info">
+                        <div class="pull-left iati-identifier-wrapper">IATI Identifier:
+                                    <span class="iati-identifier">
+                                        <!--dynamic iati identifier-->
+                                        GB-CHC-326859-GPAFINN014
+                                    </span>
+                        </div>
+                        <div class="pull-right activity-publish-state">
+                                    <span class="pull-left unpublished-in-iati">
+                                    <!--dynamic state-->
+                                        Not Published in IATI
+                                    </span>
+                            <img src="images/ic-iati-logo.png" alt="IATI" width="27" height="25">
+                        </div>
+                    </div>
+                    <div class="activity-info activity-more-info">
+                        <ul class="pull-left">
+                            <li><i class="pull-left material-icons">date_range</i>
+                                <span>
+                                    <!--dynamic date-->
+                                    01 Sep 2016 - 30 Sep 2017
+                                </span>
+                            </li>
+                            <li>
+                                <i class="pull-left material-icons">autorenew</i>
+                                <span>
+                                    <!--dynamic status-->
+                                    Implementation <i>(Status)</i></span>
+                            </li>
+                        </ul>
+                        <ul class="pull-right links">
+                            <li><a href="#"><i class="pull-left material-icons">mail</i>Contact</a></li>
+                            <li><a href="#"><i class="pull-left material-icons">share</i>Share</a></li>
+                        </ul>
+                    </div>
+                    <div class="activity-description">
+                        <p>
+                            <!--dynamic description-->
+                            This project aims to bring Inclusive Education to some of the most marginalised children
+                            using
+                            the child to child approach in the West Nile and Northern Regions of Uganda. Children are
+                            empowered to identify and engage with out of school disabled children in their communities.
+                            They
+                            will then work alongside families, teachers and communities to lobby government and advocate
+                            for
+                            inclusive education to be a priority issue and one that is main-streamed across national
+                            primary
+                            education services.
+                        </p>
+                        <span class="show-more"><i class="material-icons">more_horiz</i></span>
+                    </div>
+                    <div class="activity-sectors">
+                        <span class="pull-left">Sectors:</span>
+                        <ul class="pull-left">
+                            <!--dynamic sectors-->
+                            <li>Education<i class="pull-right material-icons">error</i>
+                                <div class="sector-more-info">
+                                    <dl>
+                                        <dt class="pull-left">Sector code:</dt>
+                                        <dd class="pull-left">Education</dd>
+                                        <dt class="pull-left">Sector vocabulary</dt>
+                                        <dd class="pull-left">1 - OECD DAC CRS Purpose Codes (5 digit)</dd>
+                                    </dl>
+                                </div>
+                            </li>
+                            <li>11220 - Primary education<i class="pull-right material-icons">error</i>
+                                <div class="sector-more-info">
+                                    <dl>
+                                        <dt class="pull-left">Sector code:</dt>
+                                        <dd class="pull-left">11220 - Primary education</dd>
+                                        <dt class="pull-left">Sector vocabulary</dt>
+                                        <dd class="pull-left">1 - OECD DAC CRS Purpose Codes (5 digit)</dd>
+                                    </dl>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="activity-block participating-organisation-block">
+                    <h2>Participating Organisations</h2>
+                    <table>
+                        <thead>
+                        <tr>
+                            <th>Organisation Name</th>
+                            <th>Organisation Type</th>
+                            <th>Organisation Role</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <!--dynamic value-->
+                        <tr>
+                            <td>DFID</td>
+                            <td>Government</td>
+                            <td>Funding</td>
+                        </tr>
+                        <tr>
+                            <td>AbleChildAfrica</td>
+                            <td>International NGO</td>
+                            <td>Accountable</td>
+                        </tr>
+                        <tr>
+                            <td>Uganda Society for Disabled Children</td>
+                            <td>National NGO</td>
+                            <td>Implementing</td>
+                        </tr>
+                        <tr>
+                            <td>Child to Child</td>
+                            <td>International NGO</td>
+                            <td>Implementing</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="activity-block transaction-block">
+                    <h2>Transaction</h2>
+                    <table>
+                        <thead>
+                        <tr>
+                            <th width="30%">Transaction Value</th>
+                            <th width="30%">Provider Receiver</th>
+                            <th width="20%">Type</th>
+                            <th width="20%">Date</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <!--dynamic value-->
+                        <tr>
+                            <td><span class="transaction-value">27,015.45 GBP</span><i>(Valued at April 01, 2015)</i>
+                            </td>
+                            <td><span class="provider"><i>circle</i>DFID</span><span class="receiver"><i>circle</i>AbleChildAfrica</span>
+                            </td>
+                            <td class="type"><strong>Incoming Fund</strong></td>
+                            <td class="date"><i class="pull-left material-icons">date_range</i>Sep 01, 2016</td>
+                        </tr>
+                        <tr>
+                            <td><span class="transaction-value">31,226.03 GBP</span><i>(Valued at April 01, 2015)</i>
+                            </td>
+                            <td><span class="provider"><i>circle</i>DFID</span><span class="receiver"><i>circle</i>AbleChildAfrica</span>
+                            </td>
+                            <td class="type"><strong>Commitment</strong></td>
+                            <td class="date"><i class="pull-left material-icons">date_range</i>Sep 01, 2016</td>
+                        </tr>
+                        <tr>
+                            <td><span class="transaction-value">27,015.45 GBP</span><i>(Valued at April 01, 2015)</i>
+                            </td>
+                            <td><span class="provider"><i>circle</i>DFID</span><span class="receiver"><i>circle</i>AbleChildAfrica</span>
+                            </td>
+                            <td class="type"><strong>Disbursement</strong></td>
+                            <td class="date"><i class="pull-left material-icons">date_range</i>Sep 01, 2016</td>
+                        </tr>
+                        <tr>
+                            <td><span class="transaction-value">31,226.03 GBP</span><i>(Valued at April 01, 2015)</i>
+                            </td>
+                            <td><span class="provider"><i>circle</i>DFID</span><span class="receiver"><i>circle</i>AbleChildAfrica</span>
+                            </td>
+                            <td class="type"><strong>Expenditure</strong></td>
+                            <td class="date"><i class="pull-left material-icons">date_range</i>Sep 01, 2016</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="activity-block budget-block">
+                    <h2>Budget</h2>
+                    <div class="budget-content">
+                        <div class="pull-left total-budget">
+                            <strong>
+                                <!--dynamic value-->
+                                11,681.57
+                            </strong>
+                            <span class="currency">GBP</span>
+                            <label>Total Budget</label>
+                        </div>
+                        <div class="pull-left budget-table">
+                            <table>
+                                <tbody>
+                                <!--dynamic value-->
+                                <tr>
+                                    <td><span class="transaction-value">27,015.45 GBP</span><i>(Valued at April 01,
+                                            2015)</i></td>
+                                    <td class="date"><i class="pull-left material-icons">date_range</i>Sep 01, 2016 -
+                                        Sep 30, 2016
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td><span class="transaction-value">74,091 GBP</span><i>(Valued at April 01,
+                                            2015)</i></td>
+                                    <td class="date"><i class="pull-left material-icons">date_range</i>Sep 01, 2016 -
+                                        Sep 30, 2016
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td><span class="transaction-value">27,015.45 GBP</span><i>(Valued at April 01,
+                                            2015)</i></td>
+                                    <td class="date"><i class="pull-left material-icons">date_range</i>Sep 01, 2016 -
+                                        Sep 30, 2016
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td><span class="transaction-value">74,091 GBP</span><i>(Valued at April 01,
+                                            2015)</i></td>
+                                    <td class="date"><i class="pull-left material-icons">date_range</i>Sep 01, 2016 -
+                                        Sep 30, 2016
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td><span class="transaction-value">27,015.45 GBP</span><i>(Valued at April 01,
+                                            2015)</i></td>
+                                    <td class="date"><i class="pull-left material-icons">date_range</i>Sep 01, 2016 -
+                                        Sep 30, 2016
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="activity-other-info">
+                <div class="pull-left updated-date"><i class="pull-left material-icons">access_time</i>Updated on
+                    <span>
+                        <!--dynamic date-->
+                        May 06, 2016 11:38
+                    </span>
+                </div>
+                <a href="#" class="view-xml-file">View XML file here</a>
+            </div>
+        </div>
+    </section>
+    <footer>Footer</footer>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+<script type="text/javascript">
+    $(document).ready(function () {
+        $('.activity-description .show-more').click(function () {
+            $(this).siblings('p').animate({
+                height: '100%'
+            });
+            $(this).hide();
+        });
+    });
+</script>
+</body>
+</html>
