f518e69277546af0e8c763e7e3793068d3e2cfdf
Sachit Bir Singh Dongol <sachit.singh@yipl.com.np>
2017-01-06 10:50:30 +0545

    Data Mapper  - [x] Structure/Flow  - [x] Interface  - [x] Logger Context provider added.

--- app/Lite/Services/Activity/ActivityService.php
+++ app/Lite/Services/Activity/ActivityService.php
@@ -1,5 +1,8 @@
 <?php namespace App\Lite\Services\Activity;
 
+use App\Lite\Services\Data\V202\Activity\Activity;
+use App\Lite\Services\Data\Traits\TransformsData;
+use App\Lite\Services\Traits\ProvidesLoggerContext;
 use Exception;
 use Psr\Log\LoggerInterface;
 use App\Lite\Contracts\ActivityRepositoryInterface;
@@ -10,10 +13,13 @@ use App\Lite\Contracts\ActivityRepositoryInterface;
  */
 class ActivityService
 {
+    use ProvidesLoggerContext, TransformsData;
+
     /**
      * @var ActivityRepositoryInterface
      */
     protected $activityRepository;
+
     /**
      * @var LoggerInterface
      */
@@ -42,16 +48,32 @@ class ActivityService
         } catch (Exception $exception) {
             $this->logger->error(
                 sprintf('Error due to %s', $exception->getMessage()),
-                [
-                    'user'     => auth()->user()->id,
-                    'userName' => auth()->user()->getNameAttribute,
-                    'trace'    => $exception->getTraceAsString()
-                ]
+                $this->getContext($exception)
             );
 
             return array();
         }
     }
 
+    /**
+     * Store the Activity data.
+     *
+     * @param array $rawData
+     * @param       $version
+     * @return \App\Models\Activity\Activity|null
+     */
+    public function store(array $rawData, $version)
+    {
+        try {
+            $activity = $this->activityRepository->save($this->transform($this->getMapping($rawData, 'Activity', $version)));
 
+            $this->logger->info('Activity successfully saved.', $this->getContext());
+
+            return $activity;
+        } catch (Exception $exception) {
+            $this->logger->error(sprintf('Error due to %s', $exception->getMessage()), $this->getContext($exception));
+
+            return null;
+        }
+    }
 }
