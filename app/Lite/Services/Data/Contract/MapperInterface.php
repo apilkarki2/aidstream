92aed1a4fd2b8aef4ac4a8cdfe02b417f6f94778
Sachit Bir Singh Dongol <sachit.singh@yipl.com.np>
2017-01-06 10:50:30 +0545

    Data Mapper  - [x] Structure/Flow  - [x] Interface  - [x] Logger Context provider added.

---
+++ app/Lite/Services/Data/Contract/MapperInterface.php
@@ -0,0 +1,15 @@
+<?php namespace App\Lite\Services\Data\Contract;
+
+/**
+ * Interface MapperInterface
+ * @package App\Lite\Services\Data\Contract
+ */
+interface MapperInterface
+{
+    /**
+     * Map raw data into the database compatible format.
+     *
+     * @return array
+     */
+    public function map();
+}
